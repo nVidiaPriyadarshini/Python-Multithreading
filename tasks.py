c38dbc0181b7fc3a777618fe1f7032355713d4e0
Vidya <vidya.p@edgenetworks.in>
2018-07-17 18:47:26 +0530

    non-blocking 2 threaded countdown simple example

---
+++ tasks.py
@@ -0,0 +1,20 @@
+import time
+import threading
+
+def countdown(count):
+	while(count >=0):
+		print("{} Counting down buddy! {}".format(threading.current_thread().name,count))
+		count -=1
+		time.sleep(3)
+def countup(count):
+        while(count <=10):
+                print("{} Counting up buddy! {}".format(threading.current_thread().name,count))
+                count +=1
+                time.sleep(5)
+
+t1 = threading.Thread(name="countdown", args=(10,), target=countdown)
+t1.start()
+
+t2 = threading.Thread(name="countup", args=(0,), target=countup)
+t2.start()
+print("All done!")
