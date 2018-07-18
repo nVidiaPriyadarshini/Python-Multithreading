4fb523f591e5e8df8063d673df618d1acfe45212
Vidya <vidya.p@edgenetworks.in>
2018-07-18 17:27:19 +0530

    amazon sqs background worker to send message

---
+++ celery-basic/tasks/sqs/config.py
@@ -0,0 +1,3 @@
+QUEUE_URL= 'https://sqs.us-east-2.amazonaws.com/910424057827/mydata-ingestor-dev'
+ACCESS_KEY='AKIAILK3PL3OER3PEZGQ'
+SECRET_KEY='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'
---
+++ celery-basic/tasks/sqs/sqs-invoker.py
@@ -0,0 +1,28 @@
+import boto3
+import json
+
+from .config import QUEUE_URL, ACCESS_KEY, SECRET_KEY
+
+sqs = boto3.client('sqs', region_name='us-east-2', aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY)
+
+def send_report_results():
+
+	payload = json.dumps({'jobId': 'test01', 'data': { 'topping': [
+			{'id': '5001', 'type': 'Chery'},
+			{'id': '5002', 'type': 'Glazed'},
+			{'id': '5003', 'type': 'Sugar'},
+			{'id': '5004', 'type': 'Powdered Sugar'},
+			{'id': '5007', 'type': 'Choclate with Sprinkles'},
+			{'id': '5010', 'type': 'Choclate'},
+			{'id': '5006', 'type': 'Maple'}]
+		}}) 
+
+	response = sqs.send_message(
+			QueueUrl= QUEUE_URL,
+			MessageBody=payload
+			)
+
+	print(response)	
+	print("SUCCESS")
+
+send_report_results()
