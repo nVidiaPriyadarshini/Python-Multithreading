1ceae01d5919dfef8383cf8d8876e6d0cafb0295
Vidya <vidya.p@edgenetworks.in>
2018-07-18 16:15:16 +0530

    celery schedule periodic task with mutex

--- celery-basic/tasks/tasks.py
+++ celery-basic/tasks/tasks.py
@@ -1,9 +1,10 @@
 from __future__ import absolute_import
 import os
+import redis
 from celery import Celery
 from celery.decorators import task
 from celery.decorators import periodic_task
-from celery.schedule import crontab
+from celery.schedules import crontab
 import time
 from datetime import timedelta
 from celery.result import AsyncResult
@@ -32,12 +33,23 @@ def data_extractor(self):
 		print('There was an exception, please retry after 5 seconds')
 		raise self.retry(exc=exc, countdown=backoff(self.request.retries))
 
-@periodic_task(run_every=timedelta(seconds=3), name="_tasks.send_mail_from_queue")
-def send_mail_from_queue():
+key = "4088587A2CAB44FD902D6D5C98CD2B17"
+
+@periodic_task(bind=True,run_every=timedelta(seconds=3), name="_tasks.send_mail_from_queue")
+def send_mail_from_queue(self):
+        REDIS_CLIENT = redis.Redis()
+	timeout = 60 * 5 # Lock expires in 5 mins"
+	have_lock = False
+	my_lock = REDIS_CLIENT.lock(key, timeout=timeout)
+
 	try:
-		message_sent= "example.email"
-		print("Email message successfully sent, [{}]".format(message_sent))
+		have_lock = my_lock.acquire(blocking=False)
+		if have_lock:
+			message_sent= "example.email"
+			print("{} Email message successfully sent, [{}]".format(self.request.hostname, message_sent))
+     			time.sleep(10)
 	finally:
 		print("release resources")
-
+		if have_lock:
+			my_lock.release()
 
