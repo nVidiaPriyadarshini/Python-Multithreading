dfab9822bea2d994bc5c109e0cccc5e62c8f25fa
nVidiaPriyadarshini <30998122+nVidiaPriyadarshini@users.noreply.github.com>
2018-07-19 14:39:42 +0530

    Update config.py

--- celery-basic/tasks/sqs/config.py
+++ celery-basic/tasks/sqs/config.py
@@ -1,4 +1,4 @@
-QUEUE_URL= 'https://sqs.us-east-2.amazonaws.com/910424057827/mydata-ingestor-dev'
-DLQ_URL= 'https://sqs.us-east-2.amazonaws.com/910424057827/myfirst-dead-letter-queue'
-ACCESS_KEY='AKIAILK3PL3OER3PEZGQ'
-SECRET_KEY='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'
+QUEUE_URL= ''
+DLQ_URL= ''
+ACCESS_KEY=''
+SECRET_KEY=''
